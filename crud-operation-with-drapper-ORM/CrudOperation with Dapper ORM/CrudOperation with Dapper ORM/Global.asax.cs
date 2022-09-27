7cc3bea694d4100db695d49dd65c0ac86edc1e08
Yogeshwaran.S <syogesh651@gmal.com>
2022-09-27 22:55:17 +0530

    Another name change

--- crud-operation-with-drapper-ORM/CrudOperation with Dapper ORM/CrudOperation with Dapper ORM/Global.asax.cs
+++ crud-operation-with-drapper-ORM/CrudOperation with Dapper ORM/CrudOperation with Dapper ORM/Global.asax.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-namespace CrudOperationUsingDapperWihtjQueryJson
+namespace CrudOperation.DapperORM
 {
     public class MvcApplication : System.Web.HttpApplication
     {
